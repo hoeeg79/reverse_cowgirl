1d291e50ecf79ce5b6cccb0d202b6a34794fbf03
hoeeg79 <93433669+hoeeg79@users.noreply.github.com>
2024-02-27 08:50:48 +0100

    ...

---
+++ integration_test/app_test.dart
@@ -0,0 +1,15 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
+// Replace with your app name
+import 'package:reverse_cowgirl/main.dart';
+
+void main() {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+  // Change name of test to something that makes sense for your app
+  testWidgets('Test MyApp', (WidgetTester tester) async {
+    await tester.pumpWidget(const MyApp());
+
+    // TODO write your test code here
+
+  });
+}
