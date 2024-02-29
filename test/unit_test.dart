86d4832f44f076970d76b8a09601a48d2a47796f
DanKjaer <114471756+DanKjaer@users.noreply.github.com>
2024-02-29 09:50:49 +0100

    Multiplication test, division test, Square root test

--- test/unit_test.dart
+++ test/unit_test.dart
@@ -4,6 +4,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:reverse_cowgirl/rpn_calc.dart';
 
 void main (){
+  //Tests for the addition command
   group('addCommand',(){
     test('Checks if the number gets added works', (){
       final stack = [1, 2];
@@ -17,7 +18,7 @@ void main (){
       expect(stack, [1, 5]);
     });
   });
-
+  //Tests for the subtraction command
   group('subtractCommand',(){
     test('Checks if the number subtraction works',(){
       final stack = [3, 4];
@@ -29,6 +30,42 @@ void main (){
       final stack = [4, 1, 2];
       SubtractCommand().apply(stack);
       expect(stack, [4, 1]);
+    });
+  });
+  //Tests for the multiplication command
+  group('multiplicationCommand', () {
+    test('Checks if the number multiplication works',(){
+      final stack = [2,2];
+      MultiplicationCommand().apply(stack);
+      expect(stack, [4]);
+    });
+
+    test('checks it only uses the two last numbers in a stack',(){
+      final stack = [2,2,2];
+      MultiplicationCommand().apply(stack);
+      expect(stack, [2, 4]);
+    });
+  });
+  //Tests for the division command
+  group('divisionCommand', () {
+    test('Checks if the number divide works',(){
+      final stack = [2.0, 4.0];
+      DivisionCommand().apply(stack);
+      expect(stack, [2.0]);
+    });
+
+    test('Checks it only uses the two last numbers in a stack',(){
+      final stack = [2.0, 4.0, 8.0];
+      DivisionCommand().apply(stack);
+      expect(stack, [2.0, 2.0]);
+    });
+  });
+
+  group('SquareRootCommand', () {
+    test('Checks if the Square root of a number works.',(){
+        final stack = [4.0];
+        SquareRootCommand().apply(stack);
+        expect(stack, [2.0]);
     });
   });
 }
