a33bfdcd80fe4d2424f1a015fc298c6b6bd4e9c7
monikabrzezinska05 <114471820+monikabrzezinska05@users.noreply.github.com>
2024-03-03 16:23:46 +0100

    widget-tests

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,6 +5,7 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
@@ -12,8 +13,66 @@ import 'package:reverse_cowgirl/main.dart';
 
 void main() {
 
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('enter-button', (WidgetTester tester) async {
     // Build our app and trigger a frame.
+    await tester.pumpWidget(const MyApp());
 
+    expect(find.text('Enter'), findsOneWidget);
+  });
+
+  testWidgets('add-button', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(const MyApp());
+
+    expect(find.text('+'), findsOneWidget);
+  });
+
+  testWidgets('substract-button', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(const MyApp());
+
+    expect(find.text('-'), findsOneWidget);
+  });
+
+  testWidgets('multiply-button', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(const MyApp());
+
+    expect(find.text('*'), findsOneWidget);
+  });
+
+  testWidgets('divide-button', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(const MyApp());
+
+    expect(find.text('/'), findsOneWidget);
+  });
+
+  testWidgets('square-root-button', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(const MyApp());
+
+    expect(find.text('√'), findsOneWidget);
+  });
+
+  testWidgets('square-button', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(const MyApp());
+
+    expect(find.text('x²'), findsOneWidget);
+  });
+
+  testWidgets('clear-button', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(const MyApp());
+
+    expect(find.text('C'), findsOneWidget);
+  });
+
+  testWidgets('undo-button', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(const MyApp());
+
+    expect(find.byIcon(Icons.undo), findsOneWidget);
   });
 }
